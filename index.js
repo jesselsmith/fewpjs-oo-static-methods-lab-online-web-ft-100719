b6532ed1b383ce142753f4631fd890be02e548dd
Jesse <jesse.lee.smith@gmail.com>
2020-02-06 23:35:25 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    return this.capitalize(string.toLowerCase().split(' ').map(e => {
+      switch (e) {
+        case "the":
+        case "a":
+        case "an":
+        case "but":
+        case "of":
+        case "and":
+        case "for":
+        case "at":
+        case "by":
+        case "from":
+          return e;
+        default: return this.capitalize(e);
+      }
+    }).join(' '))
+  }
 }
